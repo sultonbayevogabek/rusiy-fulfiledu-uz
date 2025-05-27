02a508ee1cebffcd875b6f4a3e6c29cddde0b442
sultonbayevogabek <sultonbayevogabek@gmail.com>
2025-05-27 12:39:02 +0500

    Update Telegram link in join button handler
    Replaced the outdated Telegram group URL with the correct one. This ensures users are directed to the appropriate group when the button is clicked.

--- js/register.js
+++ js/register.js
@@ -9,7 +9,7 @@
     joinButton.disabled = true;
     await statistics.onClickTgBtn();
     joinButton.disabled = false;
-    window.location.href = 'https://t.me/+dhfS5-8DCZM2NWIy';
+    window.location.href = 'https://t.me/+tELgjdWcMRgxMzUy';
   })
 
   if (user && user?.name && user?.phone && user?.time) {
